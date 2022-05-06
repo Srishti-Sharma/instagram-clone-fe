8d026a8a01b059abd7618df1049b07efcac74908
Srishti Sharma <srishtisharma85253@gmail.com>
2022-05-06 10:57:07 +0530

    feat(): adds create files

---
+++ src/screens/CreatePosts/styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core";
+
+const styles = makeStyles(() => ({
+  container: {
+    margin: "auto",
+    marginTop: 100,
+  },
+}));
+
+export default styles;
